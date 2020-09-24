ec10d7d65852f5c666e57929f8b70391e7e8f975
rala72 <code@rala.io>
2020-09-24 19:20:25 +0200

    update version
    (before release)

--- src/main/java/io/rala/Subnet.java
+++ src/main/java/io/rala/Subnet.java
@@ -14,7 +14,7 @@ import java.util.*;
  * @author rala<br>
  * <a href="mailto:code@rala.io">code@rala.io</a><br>
  * <a href="https://www.rala.io">www.rala.io</a>
- * @version 2.0.5
+ * @version 2.1.0
  */
 public class Subnet implements Comparable<@NotNull Subnet>, Iterable<@NotNull Subnet> {
     //region error messages & valid snm entries
